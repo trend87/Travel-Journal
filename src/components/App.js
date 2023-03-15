e1cfb456d44cf4eebe32ae53df6f79c4738dd977
Effiong Israel Robert <israelrobert@gmail.com>
2023-03-15 22:27:21 +0100

    use map function on Data.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from "react"
+import Header from "./Header"
+import Main from "./Main"
+import Data from "./Data"
 
 
 function App() {
+
+  const location = Data.map((item) => {
+    return <Main 
+
+            key={item.key}
+            item={item}
+    
+        
+            
+            />
+  })
+
   return(
     <div>
-      Hello World
+      <Header />
+      {location}
     </div>
   )
 }
